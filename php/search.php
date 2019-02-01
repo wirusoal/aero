4359cf6d43cd41dae79a3655c0b793bcd2cb2993
Александр Паршиков <wirusoal@yandex.ru>
2019-02-01 13:50:18 +0700

    Initial commit

---
+++ php/search.php
@@ -0,0 +1,6 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+$query = parse_url($_SERVER['REQUEST_URI'])['query'];//direction=departure&dateStart=2019-01-30T00:00:00%2B03:00&dateEnd=2019-01-31T00:00:00%2B03:00&perPage=9999&page=0&locale=ru
+$data = file_get_contents('https://www.svo.aero/bitrix/search/title_timetable/?'.$query);
+echo $data;
+?>
